df6d0cb69dced12aa5751a36980f3ee9e8bcf568
markvanm <80654019+markvanm@users.noreply.github.com>
2021-07-06 20:07:59 +0200

    Add copy to departure or arrival

--- iswim.js
+++ iswim.js
@@ -3,54 +3,54 @@ https://chrome.google.com/webstore/detail/injector/bfdonckegflhbiamlmidciapolfcc
 $("head").append('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/markvanm/itek/iswim.js"></script>');
 */
 
-$(function () {
+$(function() {
   var keyCodes = [
-    13,40, // Arrow down, enter
+    13, 40, // Arrow down, enter
     38, // Arrow up
     39, // Arrow right
     37 // Arrow left
   ];
-  $(':text').on('keydown', function (e) {
-   
-    if($(e.target).parent('div').hasClass('selector200')){
+  $(':text').on('keydown', function(e) {
+
+    if ($(e.target).parent('div').hasClass('selector200')) {
       return true;
-     }
+    }
     var keyCode = e.keyCode || e.which;
-       
+
     if (keyCodes.includes(keyCode)) { // on tab go to next input
       // prevent the default action
       e.preventDefault();
-      
+
       // select the next row containing a text input field (skip select!)
       // and get the first element
-      
+
       var currentIndex = $(e.target).closest('tr').find('input').index(e.target);
-     
-    var nextInput;
-    if(keyCode == 40 || keyCode == 13){
-    nextInput =  $(e.target).closest('tr').nextAll('tr').filter(function(index, element) {
-        return $(element).find(':text').length > 0;
-      });
-    }else if(keyCode == 38){
-    nextInput =  $(e.target).closest('tr').prevAll('tr').filter(function(index, element) {
-        return $(element).find(':text').length > 0;
-      });
-    
-    } else if(keyCode == 39){
-    nextInput =  $(e.target).closest('tr').filter(function(index, element) {
-        return $(element).find(':text').length > 0;
-      });
-      
-      currentIndex++;
-    
-    }else if(keyCode == 37){
-    nextInput =  $(e.target).closest('tr').filter(function(index, element) {
-        return $(element).find(':text').length > 0;
-      });
-      
-      currentIndex--;
-    
-    }
+
+      var nextInput;
+      if (keyCode == 40 || keyCode == 13) {
+        nextInput = $(e.target).closest('tr').nextAll('tr').filter(function(index, element) {
+          return $(element).find(':text').length > 0;
+        });
+      } else if (keyCode == 38) {
+        nextInput = $(e.target).closest('tr').prevAll('tr').filter(function(index, element) {
+          return $(element).find(':text').length > 0;
+        });
+
+      } else if (keyCode == 39) {
+        nextInput = $(e.target).closest('tr').filter(function(index, element) {
+          return $(element).find(':text').length > 0;
+        });
+
+        currentIndex++;
+
+      } else if (keyCode == 37) {
+        nextInput = $(e.target).closest('tr').filter(function(index, element) {
+          return $(element).find(':text').length > 0;
+        });
+
+        currentIndex--;
+
+      }
       nextInput = nextInput.first().find(':text');
       //console.log(nextInput.closest('tr').index());
       // if next input exists go there, else go to the first one
@@ -62,5 +62,68 @@ $(function () {
       }
     }
   });
+
+  var arrivalTable, departureTable;
+
+  if ($('input[name="currentReport.tanks[0].entity.arrivalTank.gauge"]').length) {
+    arrivalTable = $('input[name="currentReport.tanks[0].entity.arrivalTank.gauge"]').closest('table');
+    departureTable = $('input[name="currentReport.tanks[0].entity.departureTank.gauge"]').closest('table');
+
+  } else if ($('input[name="currentReport.tanks[0].entity.arrivalTank.tovMeasurement.tankGauge"]').length) {
+    arrivalTable = $('input[name="currentReport.tanks[0].entity.arrivalTank.tovMeasurement.tankGauge"]').closest('table');
+    departureTable = $('input[name="currentReport.tanks[0].entity.departureTank.tovMeasurement.tankGauge"]').closest('table');
+
+  }
+
+  function copyToDepartureOrArrival(where = 'arrival') {
+    //slob report
+
+
+    var inputs = {};
+
+    var theTable = departureTable;
+    var replaceFrom = 'departure';
+    var replaceTo = 'arrival';
+    if (where == "departure") {
+      theTable = arrivalTable;
+      replaceFrom = 'arrival';
+      replaceTo = 'departure';
+    }
+
+
+    theTable.find('input[type=text]').each(function() {
+      var keyValue = $(this).attr('name');
+      keyValue = keyValue.replace(replaceFrom, replaceTo);
+      inputs[keyValue] = $(this).val();
+    });
+
+    $.each(inputs, function(key, val) {
+      var selectName = 'input[name="' + key + '"]';
+      $(selectName).val(val);
+    });
+
+
+
+  }
+
+  if (arrivalTable) {
+
+    var button = $('<div class="button" id="uniform-undefined"><span>Kopieer naar departure ><input name="btnSelectFormGroup" tabindex="200" class="btnAddforms" type="button" value="Sort Tanks" style="opacity: 0;"></span></div>');
+
+    button.click(function() {
+      copyToDepartureOrArrival('departure');
+    });
+
+    $('#tankDIV0').append(button);
+
+    var button = $('<div class="button" id="uniform-undefined"><span>Kopieer naar arrival \<<input name="btnSelectFormGroup" tabindex="200" class="btnAddforms" type="button" value="Sort Tanks" style="opacity: 0;"></span></div>');
+
+    button.click(function() {
+      copyToDepartureOrArrival();
+    });
+
+    $('#tankDIV1').append(button);
+  }
+
 
 });
