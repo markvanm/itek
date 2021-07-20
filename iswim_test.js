c9eff87e5aa4e61e3b57e00c49e384ac6b6b69fd
markvanm <80654019+markvanm@users.noreply.github.com>
2021-07-21 01:36:10 +0200

    working calibration page

--- iswim_test.js
+++ iswim_test.js
@@ -1,70 +1,112 @@
-if($('#currentReport\\.report\\.detail\\.certifiedThermometer\\.type').length>0){
+if ($('#currentReport\\.report\\.detail\\.certifiedThermometer\\.type').length > 0) {
 
   function createCookie(name, value, days) {
-      var expires;
+    var expires;
 
-      if (days) {
-          var date = new Date();
-          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-          expires = "; expires=" + date.toGMTString();
-      } else {
-          expires = "";
-      }
-      document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    } else {
+      expires = "";
+    }
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
   }
 
   function readCookie(name) {
-      var nameEQ = encodeURIComponent(name) + "=";
-      var ca = document.cookie.split(';');
-      for (var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) === ' ')
-              c = c.substring(1, c.length);
-          if (c.indexOf(nameEQ) === 0)
-              return decodeURIComponent(c.substring(nameEQ.length, c.length));
-      }
-      return null;
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ')
+        c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0)
+        return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
   }
 
   function eraseCookie(name) {
-      createCookie(name, "", -1);
+    createCookie(name, "", -1);
   }
 
-  var obj = {
-    "certifiedThermometer.type": "currentReport\\.report\\.detail\\.certifiedThermometer\\.type",
-    "certifiedThermometer.model": "currentReport\\.report\\.detail\\.certifiedThermometer\\.model"
-  };
+  var inputArrayCalibration = [
+    "currentReport.report.detail.certifiedThermometer.type",
+    "currentReport.report.detail.certifiedThermometer.model",
+    "currentReport.report.detail.certifiedThermometer.serial",
+    "currentReport.certifiedThermometerReading",
+    "currentReport.report.detail.intertekProbe.type",
+    "currentReport.report.detail.intertekProbe.model",
+    "currentReport.intertekProbeReading",
+    "currentReport.report.detail.intertekTape.type",
+    "currentReport.report.detail.intertekTape.model",
+    "currentReport.report.detail.intertekThermometer.type",
+    "currentReport.report.detail.intertekThermometer.model",
+    "currentReport.report.detail.intertekThermometer.serial",
+    "currentReport.levelReadingIntertekTape",
+    "currentReport.levelReadingVesselProbe",
+    
+    
+    "datepicker_certifiedThermometer_intertekTape",
+    "datepicker_vesselsProbe_intertekTape",
+    
+    "datepicker_intertekTape_intertekTape1",
+    "datepicker_intertekThermometer_intertekTape1"
+
+  ];
 
 
-  function saveEq(){
-    $.each( obj, function( key, value ) {
-      createCookie(key, $('#'+value).val(), 300);
+
+  function saveEq() {
+    inputArrayCalibration.forEach((key) => {
+      var keyFormatted = key.replace(/\./g, '\\.');     
+      console.log($('#' + keyFormatted).val());
+      createCookie(key, $('#' + keyFormatted).val(), 300);
     });
   }
 
-  function loadEq(){
-    $.each( obj, function( key, value ) {
-      $('#'+value).val(readCookie(key));
+  function loadEq() {
+
+    inputArrayCalibration.forEach((key) => {
+      var keyFormatted = key.replace(/\./g, '\\.');
+      var val = readCookie(key);
+
+      if ([
+      "datepicker_certifiedThermometer_intertekTape",
+      "datepicker_vesselsProbe_intertekTape",
+      "datepicker_intertekTape_intertekTape1",
+      "datepicker_intertekThermometer_intertekTape1"].includes(key)) {
+      	var yesterday = new Date((new Date()).valueOf() - 1000*60*60*24);
+        
+        var dateYesterday = yesterday.getDate() + "/" + (yesterday.getMonth() + 1) + "/" + yesterday.getFullYear();
+        val = dateYesterday;
+      }
+      
+      console.log(val);
+      console.log(keyFormatted);
+
+      $('#' + keyFormatted).val(val).change().blur();
+
     });
+
+
   }
-  
+
   var button = $('<div class="button" id="uniform-undefined"><span>SAVE<input name="btnSelectFormGroup" tabindex="200" class="btnAddforms" type="button" value="SAVE" style="opacity: 0;"></span></div>');
 
-button.click(function() {
-  saveEq();
-});
+  button.click(function() {
+    saveEq();
+  });
 
-$('.group_800').append(button);
+  $('.group_800').append(button);
 
-button = $('<div class="button" id="uniform-undefined"><span>LOAD<input name="btnSelectFormGroup" tabindex="200" class="btnAddforms" type="button" value="LOAD" style="opacity: 0;"></span></div>');
+  button = $('<div class="button" id="uniform-undefined"><span>LOAD<input name="btnSelectFormGroup" tabindex="200" class="btnAddforms" type="button" value="LOAD" style="opacity: 0;"></span></div>');
 
-button.click(function() {
-  loadEq();
-});
+  button.click(function() {
+    loadEq();
+  });
 
-$('.group_800').append(button);
-  
-  
+  $('.group_800').append(button);
+
+
 }
-
-
